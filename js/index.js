e8521ff2b8c71e61cc36697f9a76cf322fb1af61
Duc Vu <de.yd315@gmail.com>
2019-03-05 18:02:14 -0600

    event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+const anchors = document.querySelectorAll('.nav-link');
+console.log(anchors);
+anchors.forEach( anchor => { 
+    anchor.addEventListener('mouseover', function(e){
+c        anchor.style = "font-weight: bold"
+    });
+
+    anchor.addEventListener('focus', function(){
+        anchor.style = "color: red"
+    });
+});
+
+const header = document.getElementsByTagName('header');
+header.addEventListener('wheel', function(e) {
+    console.log(e);
+
+    alert("be careful!!!");
+});
