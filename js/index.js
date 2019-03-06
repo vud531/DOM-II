628769c734d87cd7de26040f2478b0b1c6757554
Duc Vu <de.yd315@gmail.com>
2019-03-05 19:04:39 -0600

    more events listeners added

--- js/index.js
+++ js/index.js
@@ -15,17 +15,47 @@ const anchors = document.querySelectorAll('.nav-link');
 console.log(anchors);
 anchors.forEach( anchor => { 
     anchor.addEventListener('mouseover', function(e){
-c        anchor.style = "font-weight: bold"
+        anchor.style = "font-weight: bold";
     });
 
     anchor.addEventListener('focus', function(){
-        anchor.style = "color: red"
+        anchor.style = "color: red";
+    });
+
+    
+    anchor.addEventListener('blue', function(){
+        anchor.style = "color: black";
     });
 });
 
 const header = document.getElementsByTagName('header');
-header.addEventListener('wheel', function(e) {
-    console.log(e);
+header[0].addEventListener('wheel', function() {
+    console.log();
 
-    alert("be careful!!!");
+    alert("be careful!!! Don't rool the wheel in the header");
+});
+
+const logo = document.getElementsByClassName('logo-heading');
+logo[0].addEventListener('dblclick', function() {
+    console.log();
+    logo[0].innerHTML = '<h2>oh hi there, are you looking for me?<h2>';
+});
+
+
+
+
+
+// to do: go back and get this working
+const imgs = document.getElementsByTagName('img');
+imgs[1].draggable = true;
+imgs[1].addEventListener('drag', function(e) {
+    console.log(e);
+});
+
+imgs[1].addEventListener('drop', function(e) {
+    event.preventDefault();
+
+    console.log("hi");
+
+    logo[0].innerHTML = '<h2>hey, dont swap my img<h2>';
 });
